7ef33c0a5365cb69668d75fb6967bd9bf1683a14
Ubuntu <ubuntu@ip-172-31-30-241.eu-west-1.compute.internal>
2014-01-26 22:03:24 +0000

    Added main.js and some changes in index.html and main.css

---
+++ assets/js/main.js
@@ -0,0 +1,11 @@
+/*
+bootstrap_alert = function() {}
+bootstrap_alert.warning = function(message) {
+    $('#alert_placeholder').html('<div class="alert"><a class="close" data-dismiss="alert">x</a><span>'+message+'</span></div>')
+}
+*/
+
+$( "Form" ).submit(function( event ) {
+    alert("Gracias!! En breve te enviaremos el correo");
+    event.preventDefault();
+});
