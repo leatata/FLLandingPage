6d3572de0d79400cc4127614897d1ba36f0cc8ce
Ubuntu <ubuntu@ip-172-31-30-241.eu-west-1.compute.internal>
2014-02-02 00:19:05 +0000

    Add send email functionality

--- web.js
+++ web.js
@@ -3,6 +3,7 @@ var async   = require('async')
   , fs      = require('fs')
   , http    = require('http')
   , https   = require('https')
+  , nodemailer = require("nodemailer")
   , db      = require('./models');
 
 var app = express();
@@ -15,16 +16,59 @@ app.get('/', function(request, response) {
   response.send(data);
 });
 
+// create reusable transport method (opens pool of SMTP connections)
+var smtpTransport = nodemailer.createTransport("SMTP",{
+    service: "Gmail",
+    auth: {
+        user: process.env.EMAIL_USER, //"bidbidhurra@gmail.com",
+        pass: process.env.EMAIL_PWD //"alpaleje1"
+    }
+});
+
+// Read email template from file
+require.extensions['.txt'] = function (module, filename) {
+    module.exports = fs.readFileSync(filename, 'utf8');
+};
+var emailTemplate = require("./emailTemplate.txt");
+
+function sendWelcomeEmail (email) {
+    // setup e-mail data with unicode symbols
+    var mailOptions = {
+	from: "Aprende Excel <bidbidhurra@bidbidhurra.com>", // sender address
+	to: email, // list of receivers
+	subject: "Test", // Subject line
+	text: "world Hello", // plaintext body
+	html: emailTemplate // html body
+    }
+
+    // send mail with defined transport object
+    smtpTransport.sendMail(mailOptions, function(error, response){
+	if (error) {
+            console.log(error);
+	}
+	else {
+            console.log("Message sent: " + response.message);
+	}
+
+	// if you don't want to use this transport object anymore, uncomment following line
+	//smtpTransport.close(); // shut down the connection pool, no more messages
+    });    
+}
+
 // Manage email form submission
 app.post('/', function(req, res) {
     var user = global.db.user;
+    var email = req.body.email
     user
 	.create({
-	    email: req.body.email
+	    email: email
 	})
 	.complete(function(err) {
 	    if (!!err) {
 		console.log('The instance has not been saved:', err)
+	    }
+	    else {
+		sendWelcomeEmail(email);
 	    }
 	})
     res.redirect('/');
