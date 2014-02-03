ce1b39878e4462bc1cfac9016e4fd21b21538303
Ubuntu <ubuntu@ip-172-31-30-241.eu-west-1.compute.internal>
2014-02-03 00:03:08 +0000

    Add validation to Forms through JQuery validation

--- assets/js/main.js
+++ assets/js/main.js
@@ -5,35 +5,103 @@ bootstrap_alert.warning = function(message) {
 }
 */
 
-$( "Form" ).submit(function( event ) {
-//    alert("Gracias!! En breve te enviaremos el correo");
+$(document).ready(function() {
+  jQuery.validator.setDefaults({
+    errorPlacement: function(error, element) {
+      // if the input has a prepend or append element, put the validation msg after the parent div
+      if(element.parent().hasClass('input-prepend') || element.parent().hasClass('input-append')) {
+          error.insertAfter(element.parent());
+      // else just place the validation message immediatly after the input
+      } else {
+        error.insertAfter(element);
+      }
+    },
+    //errorElement: "small", // contain the error msg in a small tag
+    //wrapper: "div", // wrap the error message and small tag in a div
+    highlight: function(element) {
+      $(element).closest('.control-group').addClass('error'); // add the Bootstrap error class to the control group
+    },
+    success: function(element) {
+      $(element).closest('.control-group').removeClass('error'); // remove the Boostrap error class from the control group
+    }
+  });
+});
+
+$(document).ready(function(){
+    $('#form1').validate({
+	rules: {
+	    email: {
+		required: true,
+		email: true
+	    }
+	}
+    });
+});
+
+$(document).ready(function(){
+    $('#form2').validate({
+	rules: {
+	    email: {
+		required: true,
+		email: true
+	    }
+	}
+    });
+});
+
+$(document).ready(function(){
+    $('#form3').validate({
+	rules: {
+	    email: {
+		required: true,
+		email: true
+	    }
+	}
+    });
+});
+
+$(document).ready(function(){
+    $('#form4').validate({
+	rules: {
+	    email: {
+		required: true,
+		email: true
+	    }
+	}
+    });
+});
+
+$("Form").submit(function( event ) {
     event.preventDefault();
-    console.log("Submit form");
 });
 
 function submitForm (event) {  
     event.preventDefault();
-    var email = $(this).prevAll('div').children().val();
-    $(this).prevAll('div').children().val("");
-    $.ajax({ 
-        url: "/ajax",
-        type: "POST",
-        cache: false, 
-        data: JSON.stringify({email: email}), 
-	contentType: "application/json",
-	timeout: 5000,
-	success: function(data) {
-	    $('#modalSuccess').modal('toggle')
-	    console.log('process sucess');
-	},
-	error: function() {
-	    $('#modalError').modal('toggle')
-	    console.log('process error');
-	},
-    })
+    if ($(this).parent().parent().valid()) {
+	var email = $(this).prevAll('input').val();
+	$(this).prevAll('input').val("");
+	$.ajax({ 
+            url: "/ajax",
+            type: "POST",
+            cache: false, 
+            data: JSON.stringify({email: email}), 
+	    contentType: "application/json",
+	    timeout: 5000,
+	    success: function(data) {
+		$('#modalSuccess').modal('toggle')
+		console.log('process sucess');
+	    },
+	    error: function() {
+		$('#modalError').modal('toggle')
+		console.log('process error');
+	    },
+	})
+    }
 };
 
 $('#enter1').click(submitForm);    
 $('#enter2').click(submitForm);    
 $('#enter3').click(submitForm);    
 $('#enter4').click(submitForm);    
+
+
